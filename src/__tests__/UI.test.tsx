ae6d3c2a0a7671da74ec06110234e84e0e3a1644
Phuong Bui <buiphuongaptutc@gmail.com>
2023-11-12 15:50:48 +0700

    tests

--- src/__tests__/UI.test.tsx
+++ src/__tests__/UI.test.tsx
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../store';
 import { RouterProvider } from 'react-router-dom';
 import router from '../router';
 
-describe("snapshot", () => {
+describe("UI", () => {
     it("snapshot", async () => {
         const component = render(
             <Provider store={store}>
@@ -12,5 +12,14 @@ describe("snapshot", () => {
             </Provider>
         );
         expect(component).toMatchSnapshot();
+    });
+    it("tests login button text change on signing in", async () => {
+        await act(() => render(
+            <Provider store={store}>
+                <RouterProvider router={router} />
+            </Provider>
+        ));
+        const loginBtn = screen.getByTestId("btnLogin");
+        expect(loginBtn).toBeInTheDocument()
     });
 });
